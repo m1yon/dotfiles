953f781648a30c3efbebfdbcacbe069c0e0e7ac7
Michael Lyon <mlyon360@gmail.com>
2025-12-27 10:55:30 -0700

    remove obsidian

--- .config/nvim/plugin/40_plugins.lua
+++ .config/nvim/plugin/40_plugins.lua
@@ -29,71 +29,71 @@ local now_if_args = _G.Config.now_if_args
 --
 -- Add these plugins now if file (and not 'mini.starter') is shown after startup.
 now_if_args(function()
-	add({
-		source = "nvim-treesitter/nvim-treesitter",
-		-- Update tree-sitter parser after plugin is updated
-		hooks = {
-			post_checkout = function()
-				vim.cmd("TSUpdate")
-			end,
-		},
-	})
-	add({
-		source = "nvim-treesitter/nvim-treesitter-textobjects",
-		-- Use `main` branch since `master` branch is frozen, yet still default
-		-- It is needed for compatibility with 'nvim-treesitter' `main` branch
-		checkout = "main",
-	})
+  add({
+    source = "nvim-treesitter/nvim-treesitter",
+    -- Update tree-sitter parser after plugin is updated
+    hooks = {
+      post_checkout = function()
+        vim.cmd("TSUpdate")
+      end,
+    },
+  })
+  add({
+    source = "nvim-treesitter/nvim-treesitter-textobjects",
+    -- Use `main` branch since `master` branch is frozen, yet still default
+    -- It is needed for compatibility with 'nvim-treesitter' `main` branch
+    checkout = "main",
+  })
 
-	-- Define languages which will have parsers installed and auto enabled
-	local languages = {
-		-- These are already pre-installed with Neovim. Used as an example.
-		"lua",
-		"vimdoc",
-		"markdown",
-		"markdown_inline",
-		"go",
-		"typescript",
-		"javascript",
-		"json",
-		-- Add here more languages with which you want to use tree-sitter
-		-- To see available languages:
-		-- - Execute `:=require('nvim-treesitter').get_available()`
-		-- - Visit 'SUPPORTED_LANGUAGES.md' file at
-		--   https://github.com/nvim-treesitter/nvim-treesitter/blob/main
-	}
-	local isnt_installed = function(lang)
-		return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
-	end
-	local to_install = vim.tbl_filter(isnt_installed, languages)
-	if #to_install > 0 then
-		require("nvim-treesitter").install(to_install)
-	end
+  -- Define languages which will have parsers installed and auto enabled
+  local languages = {
+    -- These are already pre-installed with Neovim. Used as an example.
+    "lua",
+    "vimdoc",
+    "markdown",
+    "markdown_inline",
+    "go",
+    "typescript",
+    "javascript",
+    "json",
+    -- Add here more languages with which you want to use tree-sitter
+    -- To see available languages:
+    -- - Execute `:=require('nvim-treesitter').get_available()`
+    -- - Visit 'SUPPORTED_LANGUAGES.md' file at
+    --   https://github.com/nvim-treesitter/nvim-treesitter/blob/main
+  }
+  local isnt_installed = function(lang)
+    return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+  end
+  local to_install = vim.tbl_filter(isnt_installed, languages)
+  if #to_install > 0 then
+    require("nvim-treesitter").install(to_install)
+  end
 
-	-- Enable tree-sitter after opening a file for a target language
-	local filetypes = {}
-	for _, lang in ipairs(languages) do
-		for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-			table.insert(filetypes, ft)
-		end
-	end
-	local ts_start = function(ev)
-		vim.treesitter.start(ev.buf)
-	end
-	_G.Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
+  -- Enable tree-sitter after opening a file for a target language
+  local filetypes = {}
+  for _, lang in ipairs(languages) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+      table.insert(filetypes, ft)
+    end
+  end
+  local ts_start = function(ev)
+    vim.treesitter.start(ev.buf)
+  end
+  _G.Config.new_autocmd("FileType", filetypes, ts_start, "Start tree-sitter")
 end)
 
 -- add syntax highliting for .env.* files
 vim.filetype.add({
-	extension = {
-		env = "sh",
-	},
-	filename = {
-		[".env"] = "sh",
-	},
-	pattern = {
-		["%.env%.[%w_.-]+"] = "sh",
-	},
+  extension = {
+    env = "sh",
+  },
+  filename = {
+    [".env"] = "sh",
+  },
+  pattern = {
+    ["%.env%.[%w_.-]+"] = "sh",
+  },
 })
 
 -- Language servers ===========================================================
@@ -112,59 +112,59 @@ vim.filetype.add({
 --
 -- Add it now if file (and not 'mini.starter') is shown after startup.
 now_if_args(function()
-	add("neovim/nvim-lspconfig")
+  add("neovim/nvim-lspconfig")
 
-	-- Use `:h vim.lsp.enable()` to automatically enable language server based on
-	-- the rules provided by 'nvim-lspconfig'.
-	-- Use `:h vim.lsp.config()` or 'ftplugin/lsp/' directory to configure servers.
-	-- Uncomment and tweak the following `vim.lsp.enable()` call to enable servers.
-	vim.lsp.enable({
-		"gopls",
-		"lua_ls",
-		"pyright",
-		"terraformls",
-		"yamlls",
-		"tailwindcss",
-		"eslint",
-		"grammarly",
-	})
+  -- Use `:h vim.lsp.enable()` to automatically enable language server based on
+  -- the rules provided by 'nvim-lspconfig'.
+  -- Use `:h vim.lsp.config()` or 'ftplugin/lsp/' directory to configure servers.
+  -- Uncomment and tweak the following `vim.lsp.enable()` call to enable servers.
+  vim.lsp.enable({
+    "gopls",
+    "lua_ls",
+    "pyright",
+    "terraformls",
+    "yamlls",
+    "tailwindcss",
+    "eslint",
+    "grammarly",
+  })
 end)
 
 later(function()
-	add({
-		source = "pmizio/typescript-tools.nvim",
-		depends = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	})
-	require("typescript-tools").setup({})
+  add({
+    source = "pmizio/typescript-tools.nvim",
+    depends = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  })
+  require("typescript-tools").setup({})
 end)
 
 later(function()
-	add("dmmulroy/ts-error-translator.nvim")
-	require("ts-error-translator").setup({})
+  add("dmmulroy/ts-error-translator.nvim")
+  require("ts-error-translator").setup({})
 end)
 
 -- Linting ====================================================================
 later(function()
-	add("mfussenegger/nvim-lint")
-	local lint = require("lint")
-	lint.linters_by_ft = {
-		go = { "golangcilint" },
-	}
+  add("mfussenegger/nvim-lint")
+  local lint = require("lint")
+  lint.linters_by_ft = {
+    go = { "golangcilint" },
+  }
 
-	-- Create autocommand which carries out the actual linting
-	-- on the specified events.
-	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-		group = lint_augroup,
-		callback = function()
-			-- Only run the linter in buffers that you can modify in order to
-			-- avoid superfluous noise, notably within the handy LSP pop-ups that
-			-- describe the hovered symbol using Markdown.
-			if vim.bo.modifiable then
-				lint.try_lint()
-			end
-		end,
-	})
+  -- Create autocommand which carries out the actual linting
+  -- on the specified events.
+  local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+  vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    group = lint_augroup,
+    callback = function()
+      -- Only run the linter in buffers that you can modify in order to
+      -- avoid superfluous noise, notably within the handy LSP pop-ups that
+      -- describe the hovered symbol using Markdown.
+      if vim.bo.modifiable then
+        lint.try_lint()
+      end
+    end,
+  })
 end)
 
 -- Formatting =================================================================
@@ -176,34 +176,34 @@ end)
 -- The 'stevearc/conform.nvim' plugin is a good and maintained solution for easier
 -- formatting setup.
 later(function()
-	add("stevearc/conform.nvim")
+  add("stevearc/conform.nvim")
 
-	-- See also:
-	-- - `:h Conform`
-	-- - `:h conform-options`
-	-- - `:h conform-formatters`
-	require("conform").setup({
-		-- Map of filetype to formatters
-		formatters_by_ft = {
-			go = { "gofumpt", "goimports", "golines" },
-			typescriptreact = { "prettierd" },
-			typescript = { "prettierd" },
-			javascript = { "prettierd" },
-			json = { "prettierd" },
-			lua = { "stylua" },
-			python = { "ruff_fix", "ruff_format" },
-			yaml = { "prettierd" },
-			css = { "prettierd" },
-		},
+  -- See also:
+  -- - `:h Conform`
+  -- - `:h conform-options`
+  -- - `:h conform-formatters`
+  require("conform").setup({
+    -- Map of filetype to formatters
+    formatters_by_ft = {
+      go = { "gofumpt", "goimports", "golines" },
+      typescriptreact = { "prettierd" },
+      typescript = { "prettierd" },
+      javascript = { "prettierd" },
+      json = { "prettierd" },
+      lua = { "stylua" },
+      python = { "ruff_fix", "ruff_format" },
+      yaml = { "prettierd" },
+      css = { "prettierd" },
+    },
 
-		formatters = {},
+    formatters = {},
 
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	})
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  })
 end)
 
 -- Snippets ===================================================================
@@ -216,7 +216,7 @@ end)
 -- 'mini.snippets' is designed to work with it as seamlessly as possible.
 -- See `:h MiniSnippets.gen_loader.from_lang()`.
 later(function()
-	add("rafamadriz/friendly-snippets")
+  add("rafamadriz/friendly-snippets")
 end)
 
 -- Honorable mentions =========================================================
@@ -230,8 +230,8 @@ end)
 --
 -- You can use it like so:
 now_if_args(function()
-	add("mason-org/mason.nvim")
-	require("mason").setup()
+  add("mason-org/mason.nvim")
+  require("mason").setup()
 end)
 
 -- Beautiful, usable, well maintained color schemes outside of 'mini.nvim' and
@@ -249,69 +249,33 @@ end)
 
 -- AI =====================================================================
 later(function()
-	add({
-		source = "zbirenbaum/copilot.lua",
-	})
-	require("copilot").setup({
-		suggestion = {
-			enabled = true,
-			auto_trigger = true,
-			hide_during_completion = false,
-			debounce = 75,
-			trigger_on_accept = true,
-			keymap = {
-				accept = "<C-a>",
-				accept_word = false,
-				accept_line = false,
-				next = "<M-]>",
-				prev = "<M-[>",
-				dismiss = "<C-]>",
-			},
-		},
-		filetypes = {
-			yaml = true,
-		},
-	})
+  add({
+    source = "zbirenbaum/copilot.lua",
+  })
+  require("copilot").setup({
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      hide_during_completion = false,
+      debounce = 75,
+      trigger_on_accept = true,
+      keymap = {
+        accept = "<C-a>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+    filetypes = {
+      yaml = true,
+    },
+  })
 end)
 
 -- HTML/JSX =====================================================================
 later(function()
-	add("windwp/nvim-ts-autotag")
-	require("nvim-ts-autotag").setup()
-end)
-
--- Obsidian =====================================================================
-later(function()
-	add({ source = "obsidian-nvim/obsidian.nvim", depends = { "nvim-lua/plenary.nvim" } })
-	require("obsidian").setup({
-		legacy_commands = false,
-		workspaces = {
-			{
-				name = "main",
-				path = "~/obsidian",
-			},
-		},
-	})
-
-	-- Global Obsidian keymaps (always available)
-	vim.keymap.set("n", "<leader>nf", "<cmd>Obsidian quick_switch<cr>", { desc = "Files" })
-	vim.keymap.set("n", "<leader>ng", "<cmd>Obsidian search<cr>", { desc = "Grep" })
-
-	-- Buffer-local Obsidian keymaps (only in markdown files)
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = "markdown",
-		callback = function(ev)
-			local map = function(mode, lhs, rhs, desc)
-				vim.keymap.set(mode, lhs, rhs, { buffer = ev.buf, desc = desc })
-			end
-			map("n", "<leader>nn", "<cmd>Obsidian new<cr>", "Create new note")
-			map("n", "<leader>nb", "<cmd>Obsidian backlinks<cr>", "Show backlinks")
-			map("n", "<leader>nt", "<cmd>Obsidian template<cr>", "Insert template")
-			map("v", "<leader>ne", "<cmd>Obsidian extract_note<cr>", "Extract to new note")
-			map("n", "<leader>np", "<cmd>Obsidian paste_img<cr>", "Paste image")
-			map("n", "<leader>nr", "<cmd>Obsidian rename<cr>", "Rename note")
-			map("n", "<leader>nc", "<cmd>Obsidian toggle_checkbox<cr>", "Toggle checkbox")
-			map("n", "<leader>nT", "<cmd>Obsidian new_from_template<cr>", "New note from template")
-		end,
-	})
+  add("windwp/nvim-ts-autotag")
+  require("nvim-ts-autotag").setup()
 end)
