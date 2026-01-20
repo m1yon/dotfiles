7798aa9616b95c6c2f824ef93aa1a2b6ad88b0e8
Michael Lyon <mlyon360@gmail.com>
2026-01-20 15:41:39 -0700

    fix eslint format on save

--- .config/nvim/plugin/40_plugins.lua
+++ .config/nvim/plugin/40_plugins.lua
@@ -200,7 +200,7 @@ later(function()
     format_on_save = {
       -- These options will be passed to conform.format()
       timeout_ms = 500,
-      lsp_format = "fallback",
+      lsp_format = "first",
     },
   })
 end)
