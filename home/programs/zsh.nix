f6ddb6642fa6781d755b8e9586b0018bc4ef1fea
Michael Lyon <mlyon360@gmail.com>
2026-02-22 10:34:01 -0700

    add starship and rebuild

--- home/programs/zsh.nix
+++ home/programs/zsh.nix
@@ -9,7 +9,11 @@
       enable = true;
        shellAliases = {
         lg = "lazygit";
+        rebuild = "nixos-rebuild switch --flake ~/GitHub/dotfiles --use-remote-sudo";
       };
     };
+    starship = {
+        enable = true;
+      };
   };
 }
