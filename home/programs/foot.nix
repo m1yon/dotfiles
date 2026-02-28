fefae9d4260c25a6e52d4e3eb846566d9e0547d7
Michael Lyon <mlyon360@gmail.com>
2026-02-28 12:57:02 -0700

    transfer foot and hyprpaper config

--- home/programs/foot.nix
+++ home/programs/foot.nix
@@ -1,19 +1,12 @@
-{
-  config,
-  pkgs,
-  dotfilesPath,
-  ...
-}:
+{ ... }:
 
-let
-  dotfiles = "${dotfilesPath}/dotfiles";
-  outOfStore = config.lib.file.mkOutOfStoreSymlink;
-in
 {
   programs.foot = {
     enable = true;
-  };
-  home.file = {
-    ".config/foot/".source = outOfStore "${dotfiles}/foot";
+    settings = {
+      main = {
+        font = "monospace:size=11";
+      };
+    };
   };
 }
