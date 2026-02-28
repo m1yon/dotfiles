196cbf16e96d1aa1934599a32dbf2820dc098363
Michael Lyon <mlyon360@gmail.com>
2026-02-28 13:05:59 -0700

    move git config

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -16,7 +16,44 @@
 
   programs.git = {
     enable = true;
-    settings.user.name = "Michael Lyon";
-    settings.user.email = "mlyon360@gmail.com";
+    ignores = [
+      ".ignore/"
+      ".worktrees/"
+    ];
+
+    settings = {
+      user.name = "Michael Lyon";
+      user.email = "mlyon360@gmail.com";
+
+      core = {
+        editor = "nvim -f";
+        pager = "delta";
+      };
+
+      interactive.diffFilter = "delta --color-only";
+
+      merge = {
+        conflictStyle = "zdiff3";
+        ff = "only";
+      };
+
+      push.autoSetupRemote = true;
+
+      delta = {
+        navigate = true;
+        dark = true;
+        minus-style = "syntax \"#4a272f\"";
+        minus-non-emph-style = "syntax \"#4a272f\"";
+        minus-emph-style = "syntax \"#713137\"";
+        minus-empty-line-marker-style = "syntax \"#4a272f\"";
+        line-numbers-minus-style = "#914c54";
+        plus-style = "syntax \"#243e4a\"";
+        plus-non-emph-style = "syntax \"#243e4a\"";
+        plus-emph-style = "syntax \"#2c5a66\"";
+        plus-empty-line-marker-style = "syntax \"#243e4a\"";
+        line-numbers-plus-style = "#449dab";
+        line-numbers-zero-style = "#3b4261";
+      };
+    };
   };
 }
