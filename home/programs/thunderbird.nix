a0af1c8be2111f55aa624cc1016f88170745f5c1
Michael Lyon <mlyon360@gmail.com>
2026-02-28 15:27:44 -0700

    add thunderbird settings

--- home/programs/thunderbird.nix
+++ home/programs/thunderbird.nix
@@ -6,6 +6,26 @@
     profiles = {
       default = {
         isDefault = true;
+        settings = {
+          "mail.pane_config.dynamic" = 0;
+          "mail.threadpane.listview" = 1;
+          "extensions.activeThemeID" = "thunderbird-compact-dark@mozilla.org";
+          "mail.uifontsize" = 14;
+          "font.default.x-western" = "sans-serif";
+          "font.name.sans-serif.x-western" = "JetBrains Mono";
+          "font.name-list.sans-serif.x-western" = "JetBrains Mono";
+          "font.size.variable.x-western" = 14;
+          "font.name.monospace.x-western" = "JetBrains Mono";
+          "font.name-list.monospace.x-western" = "JetBrains Mono";
+          "font.size.monospace.x-western" = 14;
+          "font.default.x-unicode" = "sans-serif";
+          "font.name.sans-serif.x-unicode" = "JetBrains Mono";
+          "font.name-list.sans-serif.x-unicode" = "JetBrains Mono";
+          "font.size.variable.x-unicode" = 14;
+          "font.name.monospace.x-unicode" = "JetBrains Mono";
+          "font.name-list.monospace.x-unicode" = "JetBrains Mono";
+          "font.size.monospace.x-unicode" = 14;
+        };
       };
     };
   };
