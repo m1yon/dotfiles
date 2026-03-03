57c2e82db479ffd04d3a6cdedb2abb901f6e1bbf
Michael Lyon <mlyon360@gmail.com>
2026-03-03 10:00:25 -0700

    add wifi manager

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,6 +1,16 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
+  networking.wireless.iwd.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    impala
+  ];
+
+  environment.shellAliases = {
+    wifi = "impala";
+  };
 }
