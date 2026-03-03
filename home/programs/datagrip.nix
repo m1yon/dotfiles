19fe2d33549dfc11e7419c5a62e91831cc5dc000
Michael Lyon <mlyon360@gmail.com>
2026-03-02 17:32:38 -0700

    add ideavim config

--- home/programs/datagrip.nix
+++ home/programs/datagrip.nix
@@ -1,7 +1,20 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  dotfilesPath,
+  ...
+}:
 
+let
+  dotfiles = "${dotfilesPath}/dotfiles";
+  outOfStore = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   home.packages = [
     pkgs.jetbrains.datagrip
   ];
+
+  home.file = {
+    ".ideavimrc".source = outOfStore "${dotfiles}/datagrip/.ideavimrc";
+  };
 }
