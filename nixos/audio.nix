b47e07ba5d7f90b3c29a9f10757e8130d94cbcac
Michael Lyon <mlyon360@gmail.com>
2026-03-05 15:16:53 -0700

    add wiremix

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,6 +1,7 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = [ pkgs.wiremix ];
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
@@ -9,5 +10,23 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig.bluetoothEnhancements = {
+      "monitor.bluez.properties" = {
+        # Enable all HFP/HSP roles for hands-free microphone support.
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+        # Enable mSBC codec for wideband speech in HFP.
+        "bluez5.enable-msbc" = true;
+        # Enable SBC-XQ for higher quality A2DP.
+        "bluez5.enable-sbc-xq" = true;
+        # Enable hardware volume control on supported devices.
+        "bluez5.enable-hw-volume" = true;
+      };
+    };
   };
 }
