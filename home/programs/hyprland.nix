b607db3cb40f6baaf8bc1fa07bde9e6dbfd2ee17
Michael Lyon <mlyon360@gmail.com>
2026-03-08 10:47:17 -0600

    fix hyprpaper issue

--- home/programs/hyprland.nix
+++ home/programs/hyprland.nix
@@ -1,16 +1,13 @@
 {
-  config,
-  pkgs,
   nixConfigDir,
   ...
 }:
 
 let
   dotfiles = "${nixConfigDir}/dotfiles";
-  outOfStore = config.lib.file.mkOutOfStoreSymlink;
 in
 {
   home.file = {
-    ".config/hypr/hyprland.conf".source = outOfStore "${dotfiles}/hyprland/hyprland.conf";
+    ".config/hypr/hyprland.conf".source = "${dotfiles}/hyprland/hyprland.conf";
   };
 }
