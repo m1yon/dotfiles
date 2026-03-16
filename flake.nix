04a06c73e2ec22a7ee84956f4d384aca05f710d9
Michael Lyon <mlyon360@gmail.com>
2026-03-16 15:27:43 -0600

    remove template comments

--- flake.nix
+++ flake.nix
@@ -7,17 +7,10 @@
   };
 
   inputs = {
-    # NixOS official package source, using the nixos-25.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    # Unstable channel for packages that need newer versions
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # home-manager, used for managing user configuration
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
-      # The `follows` keyword in inputs is used for inheritance.
-      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
-      # the `inputs.nixpkgs` of the current flake,
-      # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
     llm-agents = {
@@ -71,15 +64,12 @@
       };
     in
     {
-      # Please replace my-nixos with your hostname
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs username nixConfigDir; };
         modules = [
           { nixpkgs.overlays = [ unstableOverlay inputs.claude-desktop.overlays.default ]; }
           ./nixos/configuration.nix
 
-          # make home-manager as a module of nixos
-          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
--- home/home.nix
+++ home/home.nix
@@ -16,24 +16,6 @@ in
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
-  # Import files from the current configuration directory into the Nix store,
-  # and create symbolic links pointing to those store files in the Home directory.
-
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # Import the scripts directory into the Nix store,
-  # and recursively generate symbolic links in the Home directory pointing to the files in the store.
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
   home.pointerCursor = {
     name = "rose-pine-hyprcursor";
     package = inputs.rose-pine-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default;
@@ -72,13 +54,5 @@ in
   wayland.windowManager.hyprland.portalPackage =
     if useSystemHyprland then null else hyprlandPackages.xdg-desktop-portal-hyprland;
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.11";
 }
