e7e9f62075de678cc600c3aeae6ae287f0a6f79d
Michael Lyon <mlyon360@gmail.com>
2026-03-17 09:51:41 -0600

    fix bluetooth codec

--- nixos/audio.nix
+++ nixos/audio.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.wiremix ];
+  environment.systemPackages = [ pkgs.wiremix pkgs.pulseaudio ];
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
@@ -25,7 +25,20 @@
           "hsp_ag"
           "hfp_hf"
           "hfp_ag"
+          "a2dp_sink"
+          "a2dp_source"
         ];
+        # Enable high-quality A2DP codecs (AAC, LDAC, aptX, aptX HD).
+        "bluez5.codecs" = [
+          "aac"
+          "ldac"
+          "aptx"
+          "aptx_hd"
+          "sbc"
+          "sbc_xq"
+        ];
+        # Auto-switch to HSP/HFP when mic is needed, back to A2DP when done.
+        "bluez5.autoswitch-profile" = true;
         # Enable mSBC codec for wideband speech in HFP.
         "bluez5.enable-msbc" = true;
         # Enable SBC-XQ for higher quality A2DP.
@@ -33,6 +46,23 @@
         # Enable hardware volume control on supported devices.
         "bluez5.enable-hw-volume" = true;
       };
+    };
+
+    # Default to A2DP sink profile for Bluetooth audio devices.
+    wireplumber.extraConfig.bluetoothProfile = {
+      "monitor.bluez.rules" = [
+        {
+          matches = [
+            { "device.name" = "~bluez_card.*"; }
+          ];
+          actions = {
+            update-props = {
+              "bluez5.auto-connect" = [ "a2dp_sink" ];
+              "bluez5.profile" = "a2dp-sink";
+            };
+          };
+        }
+      ];
     };
   };
 }
