71f1f86f8de418084dbd3f4c5cf3a4e869359020
Michael Lyon <mlyon360@gmail.com>
2026-03-23 14:34:43 -0600

    remove copilot from neovim

--- dotfiles/nvim/plugin/40_plugins.lua
+++ dotfiles/nvim/plugin/40_plugins.lua
@@ -244,33 +244,6 @@ end)
 --   vim.cmd('color everforest')
 -- end)
 
--- AI =====================================================================
-later(function()
-	add({
-		source = "zbirenbaum/copilot.lua",
-	})
-	require("copilot").setup({
-		suggestion = {
-			enabled = true,
-			auto_trigger = true,
-			hide_during_completion = false,
-			debounce = 75,
-			trigger_on_accept = true,
-			keymap = {
-				accept = "<C-a>",
-				accept_word = false,
-				accept_line = false,
-				next = "<M-]>",
-				prev = "<M-[>",
-				dismiss = "<C-]>",
-			},
-		},
-		filetypes = {
-			yaml = true,
-		},
-	})
-end)
-
 -- HTML/JSX =====================================================================
 later(function()
 	add("windwp/nvim-ts-autotag")
