b706cfe50698e3066b0e6f48813a5175f12e51cc
Michael Lyon <mlyon360@gmail.com>
2026-05-20 16:52:38 -0600

    fix crashing

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -8,13 +8,6 @@
   programs = {
     google-chrome = {
       enable = true;
-      commandLineArgs = [
-        "--ignore-gpu-blocklist"
-        "--enable-gpu-rasterization"
-        "--enable-zero-copy"
-        "--use-angle=vulkan"
-        "--enable-features=VaapiVideoDecoder,VaapiIgnoreDriverChecks,Vulkan,DefaultANGLEVulkan,VulkanFromANGLE,UseMultiPlaneFormatForHardwareVideo,VaapiVideoEncoder,CanvasOopRasterization"
-      ];
     };
   };
 }
