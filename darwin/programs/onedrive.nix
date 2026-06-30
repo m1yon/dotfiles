85d012227c89bf2d37eb9b6e346ee169a39f219d
Michael Lyon <mlyon360@gmail.com>
2026-06-30 14:40:39 -0600

    Add OneDrive compatibility links

--- darwin/programs/onedrive.nix
+++ darwin/programs/onedrive.nix
@@ -4,9 +4,20 @@
   homebrew.casks = [ "onedrive" ];
 
   home-manager.users.${username} =
-    { ... }:
+    { config, ... }:
+    let
+      outOfStore = config.lib.file.mkOutOfStoreSymlink;
+      cloud = "${config.home.homeDirectory}/Library/CloudStorage";
+
+      workOneDriveRoot = "${cloud}/OneDrive-MecaTherapiesLLC";
+      careCoordinatorsRoot = "${workOneDriveRoot}/Care Coordinators - Documents";
+      reportingRoot = "${workOneDriveRoot}/Reporting - Documents";
+    in
     {
-      # Add compatibility links here after OneDrive creates the real
-      # ~/Library/CloudStorage paths.
+      home.file = {
+        "OneDrive/personal".source = outOfStore workOneDriveRoot;
+        "OneDrive/carecoordinators".source = outOfStore careCoordinatorsRoot;
+        "OneDrive/reporting".source = outOfStore reportingRoot;
+      };
     };
 }
