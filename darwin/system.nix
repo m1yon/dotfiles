386646dbbe337859bba03f1c80ba3237d35c66d1
Michael Lyon <mlyon360@gmail.com>
2026-07-27 10:09:07 -0600

    Configure hot corners for all screen corners

--- darwin/system.nix
+++ darwin/system.nix
@@ -192,6 +192,10 @@ in
         "/Applications/Ghostty.app"
       ];
       show-recents = false;
+      wvous-bl-corner = 1;
+      wvous-br-corner = 1;
+      wvous-tl-corner = 1;
+      wvous-tr-corner = 1;
     };
 
     spaces = {
